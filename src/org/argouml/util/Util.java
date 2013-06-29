9a0e15548bbec6955bafe3a82802c37b81eab20e
Rodrigo Andrade <rcaa2@cin.ufpe.br>
2013-06-29 20:09:55 -0300

    uploading

---
+++ src/org/argouml/util/Util.java
@@ -0,0 +1,23 @@
+package org.argouml.util;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.runtime.internal.AroundClosure;
+
+public class Util {
+
+	public static Object proceedAroundCall(JoinPoint thisJoinPoint)
+			throws Throwable {
+		Object ret = null;
+		Object[] args = thisJoinPoint.getArgs();
+		if (args.length > 0) {
+			int i = (args.length - 1);
+			if (args[i] instanceof AroundClosure) {
+				ret = ((AroundClosure) args[i]).run(args);
+			}
+		}
+		for (Object object : args) {
+			System.out.println(object);
+		}
+		return ret;
+	}
+}
